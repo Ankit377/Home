d309667bccd047d44a7433fbc4c537e4dacaa59d
Ankit377 <westartencare@gmail.com>
2020-09-09 11:15:33 +0530

    hashrouter

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import MenuIcon from "@material-ui/icons/Menu";
 import MenuButton from "./MenuButton";
 import { makeStyles } from "@material-ui/core/styles";
-import { Link } from "react-router-dom";
+import { Link, HashRouter } from "react-router-dom";
 
 const useStyles = makeStyles(() => ({
   menuicon: {
@@ -13,35 +13,36 @@ export default function Home() {
   const classes = useStyles();
   return (
     <>
-      <MenuIcon className="d-flex flex-row m-3" />
+      <HashRouter basename="/">
+        <div className="">
+          <MenuIcon className="d-flex flex-row m-3" />
+          <Link to="/aboutyourself">
+            <MenuButton name="About Your Self" className={classes.about} />
+          </Link>
+          <Link to="/onlinecounselling">
+            <MenuButton
+              name="Online Counselling"
+              className={classes.counselling}
+            />
+          </Link>
 
-      <div className="">
-        <Link to="/aboutyourself">
-          <MenuButton name="About Your Self" className={classes.about} />
-        </Link>
-        <Link to="/onlinecounselling">
-          <MenuButton
-            name="Online Counselling"
-            className={classes.counselling}
-          />
-        </Link>
-
-        <Link to="/scholarship">
-          <MenuButton name="Scholarship" />
-        </Link>
-        <Link to="/learnonline">
-          <MenuButton name="Learn Online" />
-        </Link>
-        <Link to="/infovideos">
-          <MenuButton name="Info Videos" />
-        </Link>
-        <Link to="/mycourses">
-          <MenuButton name="My Courses" />
-        </Link>
-        <Link to="/liveinteractionclass">
-          <MenuButton name="Live Interaction Class" />
-        </Link>
-      </div>
+          <Link to="/scholarship">
+            <MenuButton name="Scholarship" />
+          </Link>
+          <Link to="/learnonline">
+            <MenuButton name="Learn Online" />
+          </Link>
+          <Link to="/infovideos">
+            <MenuButton name="Info Videos" />
+          </Link>
+          <Link to="/mycourses">
+            <MenuButton name="My Courses" />
+          </Link>
+          <Link to="/liveinteractionclass">
+            <MenuButton name="Live Interaction Class" />
+          </Link>
+        </div>
+      </HashRouter>
     </>
   );
 }
