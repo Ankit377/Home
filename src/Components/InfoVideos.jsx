e43aa5e2a9d618d3de3571650f1990b3ed6ab771
Ankit377 <westartencare@gmail.com>
2020-09-10 17:45:47 +0530

    onclick buttn windlow.lcoation

--- src/Components/InfoVideos.jsx
+++ src/Components/InfoVideos.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ButtonAppBar from "./ButtonAppBar";
 import Btn from "./Btn";
-window.onload = function () {
-  // <!--Deep link URL
-  // for existing users with app already installed on their device-- >
-  window.location = "yourapp://app.com/?screen=xxxxx";
-  // !--Download URL(TUNE link) for new users to download the app-- >
-  //setTimeout(window.location = 'http://hastrk.com/serve?action=click&publisher_id=1&site_id=2', 1000);
-};
+// window.onload = function () {
+//   // <!--Deep link URL
+//   // for existing users with app already installed on their device-- >
+//   window.location = "yourapp://app.com/?screen=xxxxx";
+//   // !--Download URL(TUNE link) for new users to download the app-- >
+//   //setTimeout(window.location = 'http://hastrk.com/serve?action=click&publisher_id=1&site_id=2', 1000);
+// };
 
 export default function InfoVideos() {
   return (
@@ -20,7 +20,12 @@ export default function InfoVideos() {
             href="https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT"
             target="blank"
           >
-            <Btn name="Germany Info Videos" />
+            <Btn
+              name="Germany Info Videos"
+              onClick={() =>
+                (window.location = "yourapp://app.com/?screen=xxxxx")
+              }
+            />
           </a>
           <a
             href="https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT"
