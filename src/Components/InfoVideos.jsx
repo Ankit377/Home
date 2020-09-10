421ec9ab33c92aa3b768a1b990786820ad8c4c46
Ankit377 <westartencare@gmail.com>
2020-09-10 17:29:08 +0530

    script inside div

--- src/Components/InfoVideos.jsx
+++ src/Components/InfoVideos.jsx
@@ -4,7 +4,7 @@ import Btn from "./Btn";
 window.onload = function () {
   // <!--Deep link URL
   // for existing users with app already installed on their device-- >
-  window.location = "yourapp://app.com/?screen=xxxxx";
+  window.location = "youtube.co";
   // !--Download URL(TUNE link) for new users to download the app-- >
   //setTimeout(window.location = 'http://hastrk.com/serve?action=click&publisher_id=1&site_id=2', 1000);
 };
@@ -20,6 +20,16 @@ export default function InfoVideos() {
             href="https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT"
             target="blank"
           >
+            window.onload = function ()
+            {
+              // <!--Deep link URL
+              // for existing users with app already installed on their device-- >
+              (window.location =
+                "https://play.google.com/store/apps/details?id=com.google.android.youtube")
+              // !--Download URL(TUNE link) for new users to download the app-- >
+              //setTimeout(window.location = 'http://hastrk.com/serve?action=click&publisher_id=1&site_id=2', 1000);
+            }
+            ;
             <Btn name="Germany Info Videos" />
           </a>
           <a
