98cd8878561012989b5bb30f1812865a8dcd68df
Ankit377 <westartencare@gmail.com>
2020-09-10 17:07:48 +0530

    deep lingking youtube

--- src/Components/InfoVideos.jsx
+++ src/Components/InfoVideos.jsx
@@ -6,13 +6,22 @@ export default function InfoVideos() {
   return (
     <>
       <ButtonAppBar title="InfoVideos" />
+
       <div>
-        <a
-          href="https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT"
-          target="blank"
-        >
-          <Btn name="Germany Info Videos" />
-        </a>
+        <div className=" " style={{ padding: "50% 0" }}>
+          <a
+            href="https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT"
+            target="blank"
+          >
+            <Btn name="Germany Info Videos" />
+          </a>
+          <a
+            href="https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT"
+            target="blank"
+          >
+            <Btn name="Germany Info Videos" />
+          </a>
+        </div>
       </div>
     </>
   );
