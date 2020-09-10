196bd5c90c1d316dfed5c79c2a91c6d5f44609ed
Ankit377 <westartencare@gmail.com>
2020-09-11 00:32:54 +0530

    intent package update

--- src/Components/InfoVideos.jsx
+++ src/Components/InfoVideos.jsx
@@ -10,6 +10,9 @@ import Iframe from "react-iframe";
 //   // !--Download URL(TUNE link) for new users to download the app-- >
 //   //setTimeout(window.location = 'http://hastrk.com/serve?action=click&publisher_id=1&site_id=2', 1000);
 // };
+setTimeout(function () {
+  window.location.replace("https://itunes.apple.com/app/id12345678");
+}, 2000);
 
 export default function InfoVideos() {
   return (
@@ -24,8 +27,11 @@ export default function InfoVideos() {
           >
             <Btn
               name="Germany Info Videos"
-              onClick={() =>
-                (window.location.replace = "yourapp://app.com/?screen=xxxxx")
+              onClick={
+                () =>
+                  (window.location.replace =
+                    "yourapp://https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT")
+                //  (window.location.replace = "yourapp://app.com/?screen=xxxxx")
               }
             />
           </a>
@@ -44,7 +50,10 @@ export default function InfoVideos() {
             <Btn name="Russia" />
           </Iframe>
 
-          <a href="intent://scan/#Intent;scheme=zxing;package=com.google.zxing.client.android;end">
+          <a
+            href="intent://scan/#Intent;scheme=zxing;package=com.google.android.youtube.player
+;end"
+          >
             <Btn name="intent" />
           </a>
         </div>
