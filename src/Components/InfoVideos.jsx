fb11b784867ecbe2574a4644cc8ba17c70c4ab70
Ankit377 <westartencare@gmail.com>
2020-09-10 15:47:59 +0530

    info videos

--- src/Components/InfoVideos.jsx
+++ src/Components/InfoVideos.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ButtonAppBar from "./ButtonAppBar";
+import Btn from "./Btn";
 
 export default function InfoVideos() {
   return (
     <>
       <ButtonAppBar title="InfoVideos" />
+      <div>
+        <a href="https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT">
+          <Btn name="Germany Info Videos" />
+        </a>
+      </div>
     </>
   );
 }
