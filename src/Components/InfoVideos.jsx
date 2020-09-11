e4af711794e55bb6b6c62a44e2afb2efd09b79f5
Ankit377 <westartencare@gmail.com>
2020-09-11 11:01:12 +0530

    new intent

--- src/Components/InfoVideos.jsx
+++ src/Components/InfoVideos.jsx
@@ -18,42 +18,25 @@ export default function InfoVideos() {
 
       <div>
         <div className=" " style={{ padding: "50% 0" }}>
-          <a
-            href="intent://https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT/#Intent;scheme=yourapp;package=com.yourapp.example"
-            target="blank"
-          >
-            <Btn
-              name="Germany Info Videos"
-              onClick={
-                () =>
-                  (window.location.replace =
-                    "yourapp://https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT")
-                //  (window.location.replace = "yourapp://app.com/?screen=xxxxx")
-              }
-            />
-            {/* setTimeout(function ()
+          <Btn
+            name="Germany Info Videos"
+            onClick={
+              () =>
+                (window.location.replace =
+                  "yourapp://https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT")
+              //  (window.location.replace = "yourapp://app.com/?screen=xxxxx")
+            }
+          />
+          {/* setTimeout(function ()
             {window.location.replace("https://itunes.apple.com/app/id12345678")}
             , 2000); */}
-          </a>
           <a
             href="https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT"
-            target="blank"
+            target="_parent"
           >
-            <Btn name="Germany Info Videos" />
+            <Btn name="Germany Info Videos1" />
           </a>
-          <Iframe
-            url="https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT"
-            id="myId"
-            className="myClassname"
-            position="relative"
-          >
-            <Btn name="Russia" />
-          </Iframe>
-
-          <a
-            href="intent://scan/#Intent;scheme=youtube;package=com.google.android.youtube.player
-;end"
-          >
+          <a href="intent://www.youtube.com/watch?v=JcxWSyowE1Q#Intent;package=com.google.android.youtube;scheme=https;end">
             <Btn name="intent" />
           </a>
         </div>
