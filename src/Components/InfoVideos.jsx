b370db68d44808554865db5586609e5ce25221e7
Ankit377 <westartencare@gmail.com>
2020-09-11 12:16:49 +0530

    default button

--- src/Components/InfoVideos.jsx
+++ src/Components/InfoVideos.jsx
@@ -3,6 +3,16 @@ import ButtonAppBar from "./ButtonAppBar";
 import Btn from "./Btn";
 
 export default function InfoVideos() {
+  function redirect() {
+    window.location.replace(
+      "yourapp://https://www.youtube.com/watch?v=whG84Y5EYaQ/"
+    );
+    setTimeout(function () {
+      window.location.replace(
+        "https://play.google.com/store/apps/details?id=com.google.android.youtube"
+      );
+    }, 2000);
+  }
   return (
     <>
       <ButtonAppBar title="InfoVideos" />
@@ -28,6 +38,11 @@ export default function InfoVideos() {
           {/* <a href="intent://www.youtube.com/watch?v=JcxWSyowE1Q#Intent;package=com.google.android.youtube;scheme=https;end">
             <Btn name="intent" />
           </a> */}
+          <a href="yourapp://https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT/#Intent;scheme=yourapp;package=com.yourapp.example">
+            <Btn name="india Info Videos" />
+          </a>
+          <Btn name="revolution" onClick={redirect} />
+          <button onClick={redirect}>default revolution button</button>
         </div>
       </div>
     </>
