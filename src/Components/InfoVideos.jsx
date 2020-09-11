95033d26a82e779a02ee476624c5d4a11fa20ec5
Ankit377 <westartencare@gmail.com>
2020-09-11 12:00:27 +0530

    default changes

--- src/Components/InfoVideos.jsx
+++ src/Components/InfoVideos.jsx
@@ -1,15 +1,6 @@
 import React from "react";
 import ButtonAppBar from "./ButtonAppBar";
 import Btn from "./Btn";
-import Iframe from "react-iframe";
-
-// window.onload = function () {
-//   // <!--Deep link URL
-//   // for existing users with app already installed on their device-- >
-//   window.location = "yourapp://app.com/?screen=xxxxx";
-//   // !--Download URL(TUNE link) for new users to download the app-- >
-//   //setTimeout(window.location = 'http://hastrk.com/serve?action=click&publisher_id=1&site_id=2', 1000);
-// };
 
 export default function InfoVideos() {
   return (
@@ -18,30 +9,25 @@ export default function InfoVideos() {
 
       <div>
         <div className=" " style={{ padding: "50% 0" }}>
-          <Btn
-            name="Germany Info Videos"
-            onClick={
-              () =>
-                (window.location.replace =
-                  "yourapp://https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT")
-              //  (window.location.replace = "yourapp://app.com/?screen=xxxxx")
-            }
-          />
-          {/* setTimeout(function ()
-            {window.location.replace("https://itunes.apple.com/app/id12345678")}
-            , 2000); */}
           <a
             href="https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT"
-            target="_parent"
+            target="blank"
           >
-            <Btn name="Germany Info Videos1" />
+            <Btn name="Germany Info Videos" />
           </a>
-          <a href="intent://www.youtube.com/watch?v=JcxWSyowE1Q#Intent;package=com.google.android.youtube;scheme=https;end">
-            <Btn name="intent" />
+
+          <a href="https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT/#Intent;scheme=yourapp;package=com.yourapp.example">
+            <Btn name="Italy Info Videos" />
           </a>
           <a href="https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT/#Intent;scheme=yourapp;package=com.yourapp.example">
-            <Btn name="Germay final change" />
+            <Btn name="Canada Info Videos" />
           </a>
+          <a href="https://www.youtube.com/watch?v=JcxWSyowE1Q&list=PLY3DMTGnFv2a0Sl1H5Qp0-X6Z8iurqSRT/#Intent;scheme=yourapp;package=com.yourapp.example">
+            <Btn name="Russia Info Videos" />
+          </a>
+          {/* <a href="intent://www.youtube.com/watch?v=JcxWSyowE1Q#Intent;package=com.google.android.youtube;scheme=https;end">
+            <Btn name="intent" />
+          </a> */}
         </div>
       </div>
     </>
