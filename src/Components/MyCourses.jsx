988e0b54298373bc17bacaf1d48f88cd16ef5628
Ankit377 <westartencare@gmail.com>
2020-09-11 14:41:14 +0530

    My courses

--- src/Components/MyCourses.jsx
+++ src/Components/MyCourses.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import ButtonAppBar from "./ButtonAppBar";
-
+import CourseCard from "./CourseCard";
+import fing from "../images/finprt.jpeg";
 export default function MyCourses() {
   return (
     <>
       <ButtonAppBar title="My Courses" />
+      <CourseCard
+        img={fing}
+        title="Basic German Course"
+        description="From zero to able to speak, write and do basic communication you can learn from this course. design in a way that you can practice after each lesson"
+      />
+      <CourseCard
+        img={fing}
+        title="Basic German Course"
+        description="From zero to able to speak, write and do basic communication you can learn from this course. design in a way that you can practice after each lesson"
+      />
+      <CourseCard
+        img={fing}
+        title="Basic German Course"
+        description="From zero to able to speak, write and do basic communication you can learn from this course. design in a way that you can practice after each lesson"
+      />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render( < React.StrictMode >
+  <
+  App / >
+  <
+  /React.StrictMode> ,
   document.getElementById("root")
 );
 
