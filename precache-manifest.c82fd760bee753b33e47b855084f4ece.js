f3cccf1d44359dd952e0150285af10b7eb264400
Ankit377 <westartencare@gmail.com>
2020-09-26 13:37:50 +0530

    Updates

---
+++ precache-manifest.c82fd760bee753b33e47b855084f4ece.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1109eb6c30ed27e9942890bf267691cd",
+    "url": "/schloarshipacedemy/index.html"
+  },
+  {
+    "revision": "7245177e72fbe4cbc93a",
+    "url": "/schloarshipacedemy/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5f25c81d0c6f3f2f505c",
+    "url": "/schloarshipacedemy/static/css/main.70ee980d.chunk.css"
+  },
+  {
+    "revision": "7245177e72fbe4cbc93a",
+    "url": "/schloarshipacedemy/static/js/2.0c880407.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/schloarshipacedemy/static/js/2.0c880407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f25c81d0c6f3f2f505c",
+    "url": "/schloarshipacedemy/static/js/main.3eafc05f.chunk.js"
+  },
+  {
+    "revision": "72343c1d922d3edd0c79",
+    "url": "/schloarshipacedemy/static/js/runtime-main.59287ae5.js"
+  },
+  {
+    "revision": "144af0327a26ca38d19f16795c5bc291",
+    "url": "/schloarshipacedemy/static/media/finprt.144af032.jpeg"
+  }
+]);
